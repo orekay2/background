365e793dba4f9f03c51ec0d11e8d16aa384abca1
orekay2 <91640990+orekay2@users.noreply.github.com>
2021-12-11 18:44:17 -0700

    p5.js files

---
+++ sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+createCanvas(1920,1080);
+  background(color(0,0,0))
+colorMode(HSB)
+  rectMode(CENTER)
+}
+var a =0
+var i = 0
+var b = true
+function draw() {
+  a+=1
+  fill(color(a,100,100) )
+  translate(mouseX,mouseY)
+  rotate(a)
+  if (i>1000 && b ==true) {
+    b = false
+  }
+  
+  if (i<-1000 && b == false) {
+   b = true 
+  }
+  if(b==false) {
+    i-=1
+  }
+  if (b == true) {
+    i+=1
+  }
+  
+  console.log(i)
+rect(i,i,100,100)
+  frameRate(480)
+}
